96948ee4a1e9f4cac28863434419d7c07ba3361a
mostafanasser775 <67934296+mostafanasser775@users.noreply.github.com>
2022-04-13 23:48:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+@st.cache(allow_output_mutation=True)
 config={
   "type": "service_account",
   "project_id": "graduation-5ebe4",
