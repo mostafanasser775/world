f2b79f16e579bb613434dbf4f4e2e018140a47ac
mostafanasser775 <67934296+mostafanasser775@users.noreply.github.com>
2022-04-14 23:12:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from transformers import RobertaTokenizer, T5ForConditionalGeneration
 import wget
 import os
 
+from streamlit_autorefresh import st_autorefresh
+st_autorefresh(interval=5 * 60 * 1000, key="dataframerefresh")
+
+def get_data():
+    # Perform some request to get a dataframe
+    client.get()
+    return df
+
+st.dataframe(get_data())
+
+
+
+
+
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
