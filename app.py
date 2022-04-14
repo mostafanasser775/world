f561278cbfd5471cb5eeb6f3d8d781abf9e7985c
mostafanasser775 <67934296+mostafanasser775@users.noreply.github.com>
2022-04-15 00:01:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from transformers import RobertaTokenizer, T5ForConditionalGeneration
 import wget
 import os
 
+from streamlit_autorefresh import st_autorefresh
 
+# Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
+# after it's been refreshed 100 times.
+count = st_autorefresh(interval=30000, limit=100, key="fizzbuzzcounter")
 
 
 
